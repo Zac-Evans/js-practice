509fb9b00a028598ceeeb15301e191b232601129
zac-evans <zacisadeveloper@gmail.com>
2020-06-02 18:32:44 -0400

    update

--- practice-2.js
+++ practice-2.js
@@ -1,3 +1,6 @@
+// Excercises can be found here: https://dc-codecamp.herokuapp.com/course/fullstack/april2017/week5/day4/aa.javascript-exercises.exercise.md
+
+
 // Hello You pt.(1 + 2)
 function hello(name) {
     if (name != null) {
@@ -63,3 +66,47 @@ function splitAmount (billAmount,serviceLevel,numberPeople) {
 }
 
 splitAmount (50,'good',4)
+
+// Print Numbers
+
+function printNumbers (start,end) {
+    for (i=start; i <= end; i++) {
+        console.log(i);
+    }
+}
+
+printNumbers (4,12);
+
+function printNumbers (start,end) {
+    while (start <= end) {
+        console.log(start);
+        start++;
+    }
+}
+
+printNumbers (4,12);
+
+
+// Print a Square
+
+function printSquare (size) {
+    for (i=0;i<size;i++) {
+        console.log ("*".repeat(size))}
+}
+
+printSquare(6);
+
+// Print a box
+
+function printBox(width,height) {
+    console.log ('*'.repeat(width))
+    for (i=0;i<(height-2);i++) {
+        if (i=0 || i=(width-1)) {
+            console.log('*')}
+        else {
+            console.log(' ');}
+    
+        console.log ('*'.repeat(width))
+}
+
+printBox(6,4);
